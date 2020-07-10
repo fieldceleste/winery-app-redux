02c402efbc4f95b8a3fa534ce2e918ed14b9138e
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-07-10 10:01:56 -0700

    pass both test in form-visible reducer

---
+++ src/_tests_/reducers/index-reducer.test.js
@@ -0,0 +1,22 @@
+import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer';
+import selectedKegReducer from '../../reducers/selected-keg-reducer';
+import * as a from './../../actions/index';
+
+
+let store = createStore(rootReducer);
+
+describe('indexReducer', () => {
+  test('Should return default state if no action type is recognized', () => {
+   expect (rootReducer({}, {type: null})).toEqual({
+     masterKegList:{},
+     formVisibleOnPage: false,
+     selectedKeg: null
+    });
+  });
+  test('initial state of kegListReducer matches root reducer', () => {
+    expect(store.getState().masterKegList).toEqual(postListReducer(undefined, {type: null}))
+  });
+});
---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import kegListReducer from './keg-list-reducer';
+import formVisibleReducer from './form-visible-reducer';
+import selectedKegReducer from './selected-keg-reducer';
+import { combineReducers } from 'redux';
+
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  masterKegList: kegListReducer,
+  selectedKeg: selectedKegReducer
+});
+
+export default rootReducer;
